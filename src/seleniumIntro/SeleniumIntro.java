d4fdc917462fa5d79a7aec60a429292cdab6d2d5
aswinsasikanth <sasikanthh@gmail.com>
2024-09-12 11:58:01 -0500

    drivers intro

---
+++ src/seleniumIntro/SeleniumIntro.java
@@ -0,0 +1,18 @@
+package seleniumIntro;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
+public class SeleniumIntro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChromeDriver driver = new ChromeDriver();
+		WebDriver driver1 = new ChromeDriver();
+		
+		driver.get("https://google.com");
+
+	}
+
+}
