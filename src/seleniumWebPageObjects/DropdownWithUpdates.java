f01295176578236fe160430f0f4085ff43d9caf9
aswinsasikanth <sasikanthh@gmail.com>
2024-09-16 11:03:18 -0500

    Dropdown where values can be updated

---
+++ src/seleniumWebPageObjects/DropdownWithUpdates.java
@@ -0,0 +1,31 @@
+package seleniumWebPageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+
+
+public class DropdownWithUpdates {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		
+		driver.findElement(By.id("divpaxinfo")).click();
+		
+		Thread.sleep(1000);
+		for (int i = 0; i<4;i++) {
+			driver.findElement(By.id("hrefIncAdt")).click();
+		}
+		
+		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+		
+		driver.findElement(By.id("btnclosepaxoption")).click();
+
+	}
+
+}
