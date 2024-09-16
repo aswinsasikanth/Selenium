6f717467225ff9321e7cb0115bf0394234c7883a
aswinsasikanth <sasikanthh@gmail.com>
2024-09-16 14:26:56 -0500

    coded another way to choose instead of using id values

--- src/seleniumWebPageObjects/DynamicDropdowns.java
+++ src/seleniumWebPageObjects/DynamicDropdowns.java
@@ -13,16 +13,30 @@ public class DynamicDropdowns {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
-		
-		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
-		driver.findElement(By.xpath("//a[@value = 'MAA']")).click();
-		
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//a[@value = 'BLR'])[2]"))).click();
-		
-		
+
+		DynamicDropdowns run = new DynamicDropdowns();
+		run.anotherWay(driver);
+//		run.oneWay(driver);
 
 	}
+
+	public void oneWay(WebDriver driver) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		driver.findElement(By.xpath("//a[@value = 'MAA']")).click();
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//a[@value = 'BLR'])[2]"))).click();
+	}
+	
+	//Another way without using IDs. Traverse from Parent-Child
+	public void anotherWay(WebDriver driver) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR']//a[@value = 'MAA']")).click();
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR']//a[@value='HYD']"))).click();
+	}
+	
 
 }
