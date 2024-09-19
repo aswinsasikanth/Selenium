c0cedbbb09d8fcd8c686786f2e9bded63eb82b38
aswinsasikanth <sasikanthh@gmail.com>
2024-09-19 15:00:20 -0500

    hover, click on search tabs, type in upper case holding shift, right click/context click

--- src/seleniumActionsAndFrames/ActionsDemo.java
+++ src/seleniumActionsAndFrames/ActionsDemo.java
@@ -1,6 +1,7 @@
 package seleniumActionsAndFrames;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -11,9 +12,14 @@ public class ActionsDemo {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.flipkart.com/");
-		
+
 		Actions action = new Actions(driver);
-		action.moveToElement(driver.findElement(By.cssSelector(".H6-NpN._3N4_BX"))).build().perform();
+		// Hover on Login button
+		action.moveToElement(driver.findElement(By.cssSelector(".H6-NpN._3N4_BX"))).contextClick().build().perform();
+
+		// Click on search tab and enter data in upper case
+		action.moveToElement(driver.findElement(By.cssSelector(".Pke_EE"))).click().keyDown(Keys.SHIFT)
+				.sendKeys("aswin").doubleClick().build().perform();
 
 	}
 
